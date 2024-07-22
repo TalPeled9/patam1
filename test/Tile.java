b14ce408a5a546572ee517160e51ac5b6030c50d
Tal Peled <talp2002@cs.colman.ac.il>
2024-07-22 16:47:54 +0300

    added tile fields and ctor

--- test/Tile.java
+++ test/Tile.java
@@ -2,6 +2,17 @@ package test;
 
 
 public class Tile {
+    public final int score;
+    public final char letter;
+
+    private Tile(int score, char letter) {
+        this.score = score;
+        this.letter = letter;
+    }
+    
+
+    
+    
 
 	
 }
