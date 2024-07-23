094553bcdc8201ad7108939a6f118cf231f06eff
Tal Peled <talp2002@cs.colman.ac.il>
2024-07-23 20:46:57 +0300

    finished word

--- test/Word.java
+++ test/Word.java
@@ -1,7 +1,55 @@
 package test;
 
+import java.util.Arrays;
 
 public class Word {
+    public Tile[] tiles;
+    public int row;
+	public int col;
+    public boolean vertical;
 
-	
+    public Word(Tile[] tiles, int row, int col, boolean vertical) {
+        this.tiles = tiles;
+        this.row = row;
+        this.col = col;
+        this.vertical = vertical;
+    }
+
+    public Tile[] getTiles() {
+        return tiles;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public boolean isVertical() {
+        return vertical;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Word other = (Word) obj;
+        if (!Arrays.equals(tiles, other.tiles))
+            return false;
+        if (row != other.row)
+            return false;
+        if (col != other.col)
+            return false;
+        if (vertical != other.vertical)
+            return false;
+        return true;
+    }
+
+    
 }
