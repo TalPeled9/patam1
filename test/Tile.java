d0c556a83d23acbc5c71e92d8eb6dad4bd594fa3
Tal Peled <talp2002@cs.colman.ac.il>
2024-07-30 19:16:14 +0300

    fixed totalTiles

--- test/Tile.java
+++ test/Tile.java
@@ -113,7 +113,9 @@ public class Tile {
             };
             tilesCounter = new int[] {9,2,2,4,12,2,3,2,9,1,1,4,2,6,8,2,1,6,4,6,4,2,2,1,2,1};
             maxTilesCounter = tilesCounter.clone();
-            totalTiles = 98;
+            totalTiles = 0;
+            for (int i : maxTilesCounter)
+                totalTiles += i;
             
         }
 
