5a8309abd1edf2ea859f714b4a727030b0cdb656
Tal Peled <talp2002@cs.colman.ac.il>
2024-08-12 18:27:43 +0300

    fdone assignment2 - 100

--- test/BloomFilter.java
+++ test/BloomFilter.java
@@ -29,9 +29,9 @@ public class BloomFilter {
     private int getHashValue(String word, MessageDigest md){
         byte[] bytes = word.getBytes();
         byte[] hash = md.digest(bytes);
-        BigInteger hashBigInteger = new BigInteger(1, hash);
-        int hashValue = hashBigInteger.mod(BigInteger.valueOf(bitSetSize)).intValue();
-        return hashValue;
+        BigInteger hashBigInteger = new BigInteger(hash);
+        int index = Math.abs(hashBigInteger.intValue())%this.bitSetSize;
+        return index;
     }
 
     public void add(String word) {
@@ -49,8 +49,8 @@ public class BloomFilter {
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder(bitSetSize);
-        for (int i = 0; i < bitSetSize; i++) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < bitSet.length(); i++) {
             sb.append(bitSet.get(i) ? '1' : '0');
         }
         return sb.toString();
